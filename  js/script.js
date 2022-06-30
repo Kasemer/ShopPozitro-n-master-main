a5c143f457ceb89e04122613ffe7758ff7105215
Kasemer <34jojojo@gmail.com>
2022-06-30 10:37:56 +0300

    m

---
+++  js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+$(".t1").click(function () {
+$(".room").css("background-image","url(../img/pic1.svg)");
+$(".tt1").css("display","block");
+$(".tt2, .tt3, .tt4, .tt5, .tt6, .tt7, .tt8, .tt9").css("display","none");
+
+
+});
+
+$(".t2").click(function () {
+$(".room").css("background-image","url(../img/pic2.svg)");
+$(".tt2").css("display","block");
+$(".tt1, .tt3, .tt4, .tt5, .tt6, .tt7, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t3").click(function () {
+$(".room").css("background-image","url(../img/pic3.svg)");
+$(".tt3").css("display","block");
+$(".tt2, .tt1, .tt4, .tt5, .tt6, .tt7, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t4").click(function () {
+$(".room").css("background-image","url(../img/pic4.svg)");
+$(".tt4").css("display","block");
+$(".tt2, .tt3, .tt1, .tt5, .tt6, .tt7, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t5").click(function () {
+$(".room").css("background-image","url(../img/pic5.svg)");
+$(".tt5").css("display","block");
+$(".tt2, .tt3, .tt4, .tt1, .tt6, .tt7, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t6").click(function () {
+$(".room").css("background-image","url(../img/pic6.svg)");
+$(".tt6").css("display","block");
+$(".tt2, .tt3, .tt4, .tt5, .tt1, .tt7, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t7").click(function () {
+$(".room").css("background-image","url(../img/pic7.svg)");
+$(".tt7").css("display","block");
+$(".tt2, .tt3, .tt4, .tt5, .tt6, .tt1, .tt8, .tt9").css("display","none");
+
+});
+
+$(".t8").click(function () {
+$(".room").css("background-image","url(../img/pic8.svg)");
+$(".tt8").css("display","block");
+$(".tt2, .tt3, .tt4, .tt5, .tt6, .tt7, .tt1, .tt9").css("display","none");
+
+});
+
+$(".t9").click(function () {
+$(".room").css("background-image","url(../img/pic9.svg)");
+$(".tt9").css("display","block");
+$(".tt2, .tt3, .tt4, .tt5, .tt6, .tt7, .tt8, .tt1").css("display","none");
+
+});
+
+
+
+
+
+});
